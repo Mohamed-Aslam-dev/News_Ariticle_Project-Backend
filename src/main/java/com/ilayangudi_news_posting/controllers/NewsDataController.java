476f1313f4b4213b8632a4e273f7726e991a1a74
Mohamed-Aslam-dev <aslam93615@gmail.com>
2025-08-30 00:57:47 +0530

    commit to post method

--- src/main/java/com/ilayangudi_news_posting/controllers/NewsDataController.java
+++ src/main/java/com/ilayangudi_news_posting/controllers/NewsDataController.java
@@ -2,6 +2,7 @@ package com.ilayangudi_news_posting.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ilayangudi_news_posting.dto.NewsDataDTO;
 import com.ilayangudi_news_posting.servicerepo.NewsDataServiceRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/news")
 public class NewsDataController {
